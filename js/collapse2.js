8ac3e7d158452a126a4b4d242e3a977107db6a17
Sydol ADOMADE <81626954+sydol26@users.noreply.github.com>
2024-01-22 20:42:13 +0100

    Delete js directory

--- js/collapse2.js
+++
@@ -1,6 +0,0 @@
-// Pour la deuxième section
-$(document).ready(function() {
-    $('#accordionExample2 .collapse').on('show.bs.collapse', function() {
-        $('#accordionExample2 .collapse.show').not(this).collapse('hide');
-    });
-});
